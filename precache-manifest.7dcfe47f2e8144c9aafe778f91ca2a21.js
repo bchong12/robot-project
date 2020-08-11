0cd1cc5a3bf1056a841aaf1ec4d6663fab0d8db9
Branden Chong <brandenchong@Brandens-MacBook-Air.local>
2020-08-11 12:23:15 -0700

    Updates

---
+++ precache-manifest.7dcfe47f2e8144c9aafe778f91ca2a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8f574eac2861c6cec4b986edfe54f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680a94c148c5cdadb178",
+    "url": "/static/css/main.0f2b566b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680a94c148c5cdadb178",
+    "url": "/static/js/main.f107c67a.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
